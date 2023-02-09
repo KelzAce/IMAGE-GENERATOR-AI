6446f0d4c19895445e1b4eda11b81fc625646afb
ikechi Greatness <ikechigreat@gmail.com>
2023-02-10 00:34:07 +0100

    Frontend Development

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 port = process.env.PORT || 5000;
@@ -9,6 +10,9 @@ const app = express();
 //Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/openai", openaiRoute);
 
